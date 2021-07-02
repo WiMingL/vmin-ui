00c4c9e6edd47fa46e49a463be0bdf30cea61f2f
ViMin <lwmdyx1784329@163.com>
2021-07-02 15:38:58 +0800

    button component ui

---
+++ packages/button/src/button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button class="vm-btn"
+          :class="{'vm-disable': disabled}"
+          :style="{
+            fontSize: fontSize + 'px'
+          }"
+          :disabled="disabled"
+          @click="click">
+    {{ value }}
+  </button>
+</template>
+
+<script>
+import './button.css'
+export default {
+  name: 'VmButton',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: String,
+      default: '按钮'
+    },
+    fontSize: {
+      type: Number,
+      default: 14
+    },
+    load: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
